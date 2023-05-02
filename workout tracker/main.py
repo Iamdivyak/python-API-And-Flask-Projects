d0b6b6f7e3ccebb004539a70eddc6ffa6e5ceb65
Divya Kumari <102896170+Iamdivyak@users.noreply.github.com>
2023-05-03 01:52:12 +0530

    debuged some parts

--- workout tracker/main.py
+++ workout tracker/main.py
@@ -1,61 +1,47 @@
 import requests
-from datetime import datetime
 import os
-APP_ID = os.environ.get("API_ID")
-API_KEY = os.environ.get("API_KEY")
-GENDER = "female"
-WEIGHT_KG = 66.00
-HEIGHT_CM = 165
-AGE = 20
-headers = {
-    "x-app-id": APP_ID,
+import datetime
+
+API_ID = os.environ["api_id"]
+API_KEY = os.environ["api_key"]
+
+nutritionix_header = {
+    "x-app-id": API_ID,
     "x-app-key": API_KEY,
-    "x-remote-user-id": "0",
-}
-sheety_endpoint = os.environ.get("sheety_endpoint")
-
-exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-params = {
-    "query": input("Tell which exercise you did today?: "),
-    "gender":GENDER,
-    "weight_kg":WEIGHT_KG,
-    "height_cm":HEIGHT_CM,
-    "age":AGE,
-    # "x-app-id": APP_ID,
-    # "x-app-key": API_KEY,
+    # "x-remote-user-id": 0,
 }
 
-response = requests.post(exercise_endpoint, headers=headers, json=params)
-response.raise_for_status()
-result = response.json()
+nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+nutritionix_params = {
+    "query": input("Tell me which exercises you did Today?: "),
+    "gender": "female",
+    "weight_kg": 66,
+    "height_cm": 167,
+    "age": 22
+}
+
+nutritionix_response = requests.post(url=nutritionix_endpoint, headers=nutritionix_header, json=nutritionix_params)
+result = nutritionix_response.json()
 print(result)
 
-exercise = result["exercises"][0]["name"].title()
-duration = result["exercises"][0]["duration_min"]
-calories = result["exercises"][0]["nf_calories"]
+sheety_endpoint = os.environ["sheety_endpoint"]
 
+now = datetime.datetime.now()
+date = now.date().strftime("%d/%m/%Y")
+time = now.time().strftime("%X")
+# print(date, time)
 
-today_date = datetime.now().strftime("%d/%m/%Y")
-now_time = datetime.now().strftime("%X")
-sheety_header = {"Authorization":f"Bearer {os.environ.get('token')}"}
-
-sheet_inputs = {
-    "workout": {
-        "Date": today_date,
-        "Time": now_time,
-        "Exercise": exercise,
-        "Duration": duration,
-        "Calories": calories
+for exercise in result["exercises"]:
+    sheety_params = {
+        "sheet1": {
+            "date": date,
+            "time": time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
     }
-}
 
-sheet_response = requests.post(sheety_endpoint, json=sheet_inputs, headers=sheety_header)
-sheet_response.raise_for_status()
+sheet_response = requests.post(url=sheety_endpoint, json=sheety_params)
 print(sheet_response.text)
-
-
-
-
-
-
-
